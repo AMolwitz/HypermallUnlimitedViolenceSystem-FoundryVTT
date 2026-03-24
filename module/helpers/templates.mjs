4a72fbdffc24e5b492b9f1eb489386403ea9616d
AMolwitz <alexmolwitz@gmail.com>
2026-03-24 16:32:00 -0400

    Reformatted actor sheets

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -13,10 +13,8 @@ export const preloadHandlebarsTemplates = async function () {
     "actor-mutations": "systems/hypermalluv/templates/actor/parts/actor-mutations.html",
     "actor-psionics": "systems/hypermalluv/templates/actor/parts/actor-psionics.html",
     "actor-passions": "systems/hypermalluv/templates/actor/parts/actor-passions.html",
-    "actor-foundry-data": "systems/hypermalluv/templates/actor/parts/actor-foundry-data.html",
     "actor-gear-list": "systems/hypermalluv/templates/actor/parts/actor-gear-list.html",
     "actor-effects": "systems/hypermalluv/templates/actor/parts/actor-effects.html",
-//systems/HMUV/templates/actor/parts/actor-foundry-data.html
     // App partials.
   };
 
